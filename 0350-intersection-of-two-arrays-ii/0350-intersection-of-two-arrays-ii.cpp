852b7502edf1e647dc5d339910e97a3bda1ac224
Kapish Gupta <85850675+kapishgupta99@users.noreply.github.com>
2023-06-09 04:33:58 +0530

    Time: 12 ms (14.94%), Space: 10.7 MB (17.13%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+       std::unordered_map<int, int> freqMap;
+    std::vector<int> result;
+
+    // Store the frequency of elements in nums1
+    for (int num : nums1) {
+        freqMap[num]++;
+    }
+
+    // Check the frequency of elements in nums2
+    // and add them to the result vector accordingly
+    for (int num : nums2) {
+        if (freqMap[num] > 0) {
+            result.push_back(num);
+            freqMap[num]--;
+        }
+    }
+
+    return result;
+    }
+};
