a4804334954844295b54f36f8c5cb889b67afa29
Kapish Gupta <85850675+kapishgupta99@users.noreply.github.com>
2023-06-21 19:40:19 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (91.58%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
@@ -10,19 +10,19 @@
  * };
  */
 class Solution {
-public:
-   TreeNode * inorder(TreeNode *root,TreeNode *tmp)
-    {
-        if(root==NULL) return tmp;
+
+    TreeNode* inorder(TreeNode *root, TreeNode *tmp){
+        if(root==NULL)return tmp;
         tmp=inorder(root->left,tmp);
         tmp->right=root;
         root->left=NULL;
         tmp=root;
         return inorder(root->right,tmp);
+
     }
     
-    TreeNode* increasingBST(TreeNode* root) 
-    {
+    public:
+    TreeNode* increasingBST(TreeNode* root) {
         TreeNode* dummy=new TreeNode(0);
         TreeNode* tmp=dummy;
         inorder(root,tmp);
